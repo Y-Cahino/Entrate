f2bb775f63ec8ca93c0100203b7efd9133452b94
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-03-06 13:35:39 +0100

    Aggiungere i file di progetto.

---
+++ Entrate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int[] a = new int[13];
+            for(int i = 1; i < a.Length; i++)
+            {
+                Console.WriteLine("Inserire entrata mensile: ");
+                a[i] = int.Parse(Console.ReadLine());
+                x+=a[i];
+            }
+            Console.WriteLine("Le entrate totali ammontano a: "+x);
+        }
+    }
+}
